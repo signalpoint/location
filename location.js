b3269f82973218ddd0ac7ad78081569d5cccabc5
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-03-06 16:45:43 -0500

    Add theme_location().

--- location.js
+++ location.js
@@ -1,1 +1,21 @@
+/**
+ * Theme a location.
+ */
+function theme_location(variables) {
+  try {
+    var html = '';
+    if (variables.street) { html += variables.street + '<br />'; }
+    if (variables.additional) { html += variables.additional + '<br />'; }
+    if (variables.city) { html += variables.city + ''; }
+    if (variables.province) {
+      if (variables.city) { html += ', '; }
+      html += variables.province + ' ';
+    }
+    if (variables.postal_code) { html += variables.postal_code + '<br />'; }
+    else { html += '<br />'; }
+    if (variables.country) { html += variables.country + '<br />'; }
+    return html;
+  }
+  catch (error) { console.log('theme_location - ' + error); }
+}
 
